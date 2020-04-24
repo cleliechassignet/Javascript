e4aa4b71b05cacebc776bf4e2b9223b9486c2898
zouuh <zoedur@gmail.com>
2020-04-24 14:11:59 +0200

    Rectification state

--- src/state/index.js
+++ src/state/index.js
@@ -1,71 +1,71 @@
 export default {
     count: 0,
 
-    // page: [{
-    //         titre: 'Général',
-    //         d_1: {
-    //             titre2: 'Nombre d\'arbres',
-    //             rows: 205245
-    //         },
-    //         d_2: {
-    //             'Libellé',
-    //             value: [],
-    //             count: []
-    //         },
-    //         d_3: {
-    //             'Localisation',
-    //             value: [],
-    //             count: []
-    //         },
-    //         d_4: {
-    //             'Domanialité',
-    //             value: [],
-    //             count: []
-    //         },
-    //         d_5: {
-    //             'Hauteur (m)',
-    //             value: [],
-    //             count: []
-    //         },
-    //         d_6: {
-    //             'Circonférence (cm)',
-    //             value: [],
-    //             count: []
-    //         }
-    //     },
-    //     {
-    //         titre: {
-    //             'Géographique',
-    //             value: [],
-    //             count: []
-    //         },
-    //         d_1: {
-    //             'Localisation',
-    //             value: [],
-    //             count: []
-    //         },
-    //         d_2: {
-    //             'Domanialité'
-    //             value: [],
-    //             count: []
-    //         }
-    //     },
-    //     {
-    //         titre: {
-    //             'Caractéristiques',
-    //             value: [],
-    //             count: []
-    //         },
-    //         d_1: {
-    //             'Libellé',
-    //             value: [],
-    //             count: []
-    //         },
-    //         d_2: {
-    //             'Hauteur et circonférence'
-    //             value: [],
-    //             count: []
-    //         }
-    //     }
-    // ]
+    page: [{
+            titre: 'Général',
+            d_1: {
+                label: 'Nombre d\'arbres',
+                rows: 205245
+            },
+            d_2: {
+                label: 'Libellé',
+                value: [],
+                count: []
+            },
+            d_3: {
+                label: 'Localisation',
+                value: [],
+                count: []
+            },
+            d_4: {
+                label: 'Domanialité',
+                value: [],
+                count: []
+            },
+            d_5: {
+                label: 'Hauteur (m)',
+                value: [],
+                count: []
+            },
+            d_6: {
+                label: 'Circonférence (cm)',
+                value: [],
+                count: []
+            }
+        },
+        {
+            titre: {
+                label: 'Géographique',
+                value: [],
+                count: []
+            },
+            d_1: {
+                label: 'Localisation',
+                value: [],
+                count: []
+            },
+            d_2: {
+                label: 'Domanialité'
+                value: [],
+                count: []
+            }
+        },
+        {
+            titre: {
+                label: 'Caractéristiques',
+                value: [],
+                count: []
+            },
+            d_1: {
+                label: 'Libellé',
+                value: [],
+                count: []
+            },
+            d_2: {
+                label: 'Hauteur et circonférence'
+                value: [],
+                count: []
+            }
+        }
+    ]
 }
